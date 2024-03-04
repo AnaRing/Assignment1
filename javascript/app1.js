377ffc58a7b9483bc390b79afb1a95eaa2838c79
Ana <anaringom@gmail.com>
2024-03-04 12:40:18 +0100

    added javascript and styles for planets

--- javascript/app1.js
+++ javascript/app1.js
@@ -1,55 +1,57 @@
 // asynch fetch
-const fetchMovies = async()=> {
-    const response = await fetch('https://swapi.dev/api/films/');
+const fetchPlanets = async()=> {
+    const response = await fetch('https://swapi.dev/api/planets/');
     const result = await response.json();
     renderData(result.results);
 }  
 
-fetchMovies();
+fetchPlanets();
 
-function renderData(movies){
-    // sorting movies on episode id
-    movies.sort((a, b) => a.episode_id - b.episode_id);
+function renderData(planets){
     const ul = document.querySelector('.main__container');
     document.body.appendChild(ul);
 
-    movies.forEach(movie => {
-        console.log(movie);
+    planets.forEach(planet => {
+        console.log(planet);
         // create elements
         const li = document.createElement('li'); 
-        const movieTitle = document.createElement('span');
-        const movieEpisode = document.createElement('span');
-        const movieDirector = document.createElement('span');
-        const movieProducer = document.createElement('span');
-        const movieRelease = document.createElement('span');
-        const movieCover = document.createElement('span');
-        const movieImage = document.createElement('img');
+        const planetName = document.createElement('span');
+        const planetRotation = document.createElement('span');
+        const planetOrbit = document.createElement('span');
+        const planetSize = document.createElement('span');
+        const planetClimate = document.createElement('span');
+        const planetTerrain = document.createElement('span');
+        const planetPicture = document.createElement('span');
+        const planetImage = document.createElement('img');
 
         // append elements
         ul.append(li);
-        li.appendChild(movieTitle);
-        li.appendChild(movieEpisode);
-        li.appendChild(movieDirector);
-        li.appendChild(movieProducer);
-        li.appendChild(movieRelease);
-        li.appendChild(movieCover);
+        li.appendChild(planetName);
+        li.appendChild(planetRotation);
+        li.appendChild(planetOrbit);
+        li.appendChild(planetSize);
+        li.appendChild(planetClimate);
+        li.appendChild(planetTerrain);
+        li.appendChild(planetPicture);
 
         // making content
-        movieTitle.textContent = movie.title;
-        movieEpisode.textContent = movie.episode_id;
-        movieDirector.textContent = movie.director;
-        movieProducer.textContent = movie.producer;
-        movieRelease.textContent = movie.release_date;
-        /* movieCover.src =  */
+        planetName.textContent = planet.name;
+        planetRotation.textContent = planet.rotation_period;
+        planetOrbit.textContent = planet.orbital_period;
+        planetSize.textContent = planet.diameter;
+        planetClimate.textContent = planet.climate;
+        planetTerrain.textContent = planet.terrain;
+        /* planetImage.src =  */
 
         // adding classes to elements
         li.classList.add('created__Li');
-        movieTitle.classList.add('movie__Title');
-        movieEpisode.classList.add('movie__Episode');
-        movieDirector.classList.add('movie__Director');
-        movieProducer.classList.add('movie__Producer');
-        movieRelease.classList.add('movie__Release-date');
-        movieCover.classList.add('movie__Cover');
+        planetName.classList.add('planet__Name');
+        planetRotation.classList.add('planet__Rotation');
+        planetOrbit.classList.add('planet__Orbit');
+        planetSize.classList.add('planet__Size');
+        planetClimate.classList.add('planet__Climate');
+        planetTerrain.classList.add('planet__Terrain');
+        planetImage.classList.add('planet__Image');
 
     });
 }
